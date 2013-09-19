0b3d888c7aa8ffef7f72f4643c7acf8a229de6e9
Nick <nickcox61@gmail.com>
2013-09-19 16:44:14 -0400

    Simple Servo (working)

--- src/SimpleServo.cpp
+++ src/SimpleServo.cpp
@@ -17,14 +17,51 @@ int main(int argc, char** argv){
         exit(1);
     }
     
-    maestro.SetBaudRate(SerialStreamBuf::BAUD_115200);
+    maestro.SetBaudRate(SerialStreamBuf::BAUD_57600);
+    if (!maestro.good())
+    {
+        std::cerr << "[" << __FILE__ << ":" << __LINE__ << "] "
+                  << "Error: Could not set baud rate."
+                  << std::endl;
+        exit(1);
+    }
     maestro.SetCharSize(SerialStreamBuf::CHAR_SIZE_8);
+    if (!maestro.good())
+    {
+        std::cerr << "[" << __FILE__ << ":" << __LINE__ << "] "
+                  << "Error: Could not set char size."
+                  << std::endl;
+        exit(1);
+    }
     maestro.SetNumOfStopBits(1);
+    if (!maestro.good())
+    {
+        std::cerr << "[" << __FILE__ << ":" << __LINE__ << "] "
+                  << "Error: Could not set num stop bits."
+                  << std::endl;
+        exit(1);
+    }
     maestro.SetParity(SerialStreamBuf::PARITY_NONE);
+    if (!maestro.good())
+    {
+        std::cerr << "[" << __FILE__ << ":" << __LINE__ << "] "
+                  << "Error: Could not set parity."
+                  << std::endl;
+        exit(1);
+    }
     maestro.SetFlowControl(SerialStreamBuf::FLOW_CONTROL_NONE);
+    if (!maestro.good())
+    {
+        std::cerr << "[" << __FILE__ << ":" << __LINE__ << "] "
+                  << "Error: Could not set flow control."
+                  << std::endl;
+        exit(1);
+    }
 
-    char cmd[] = {0xAA, 0x0C, 0x04, 0x04, 0x70, 0x2E};
-    maestro.write(cmd, sizeof(cmd));
-    
+    while(1){
+        char cmd[] = {0x84, 0x00, 0x00, 0x3E};
+        maestro.write(cmd, sizeof(cmd));
+        usleep(1000000);
+    }
     maestro.Close();
 }
