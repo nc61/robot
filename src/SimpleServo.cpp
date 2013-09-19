e7eb3cb1ec22a8dcaf9478ed231dc9cd9e97882a
Nick <nickcox61@gmail.com>
2013-09-19 19:24:38 -0400

    Updated for lab

--- src/SimpleServo.cpp
+++ src/SimpleServo.cpp
@@ -2,11 +2,19 @@
 #include <string>
 #include <iostream>
 #include <stdlib.h>
+#include <math.h>
+#include <ros/ros.h>
+
 using namespace LibSerial;
 
+float getPos();
+SerialStream maestro;
+
 int main(int argc, char** argv){
-     
-    SerialStream maestro;
+    
+    ros::init(argc, argv, "lab3");
+    ros::NodeHandle nh;
+    
     maestro.Open("/dev/ttyACM0");
 
     if (!maestro.good())
@@ -57,11 +65,57 @@ int main(int argc, char** argv){
                   << std::endl;
         exit(1);
     }
+    
+    char cmd_speed[] = {0x84, 0x04, 0xE4, 0x00};
+    float position = 0;
+    ros::Rate update_rate(20);
+    
+    while(ros::ok()){
+        char cmd_max[] = {0x84, 0x04, 0x40, 0x3E};
+        
+        maestro.write(cmd_max, sizeof(cmd_max));
+        
+        while (!(abs(position - 45) < .1))
+        {
+            position = getPos();
+            ROS_INFO("Current position: %f", getPos());
+            update_rate.sleep();
 
-    while(1){
-        char cmd[] = {0x84, 0x00, 0x00, 0x3E};
-        maestro.write(cmd, sizeof(cmd));
-        usleep(1000000);
+        }
+        
+        char cmd_min[] = {0x84, 0x04, 0x68, 0x07};
+        
+        maestro.write(cmd_min, sizeof(cmd_min));
+        
+        while (!(abs(position + 45) < .1)) 
+        {
+            position = getPos();
+            ROS_INFO("Current position: %f", getPos());
+            update_rate.sleep();
+        }
+
     }
     maestro.Close();
+}
+
+float getPos()
+{
+    char cmd[] = {0x90, 0x04};
+    maestro.write(cmd, sizeof(cmd));
+
+    char response[2];
+    maestro.read(response, sizeof(response));
+
+    float pulse_length = (response[0] + 256*response[1])/4000;
+    float position = (90)*(pulse_length - 1.5);
+
+    if (!maestro.good())
+    {
+        std::cerr << "[" << __FILE__ << ":" << __LINE__ << "] "
+                  << "Error: Serial error in getPos()."
+                  << std::endl;
+        exit(1);
+    }
+
+    return position;
 }
