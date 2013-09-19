59be8a12484743284fb933a7355bd2fee12e0425
Nick <nickcox61@gmail.com>
2013-09-19 10:11:31 -0400

    Cleaned it up a bit and added function prototype

--- src/Maestro.cpp
+++ src/Maestro.cpp
@@ -13,7 +13,7 @@
 #define SERVO_BUCKET 4
 #define SERVO_ARM_RAISE_POS 0x3E40
 #define SERVO_ARM_BASE_POS 0x2E70
-#define SERVO_BUCKET_RAISE_POS 0x3E40
+#define SERVO_BUCKET_LIFT_POS 0x3E40
 #define SERVO_BUCKET_BASE_POS 0x2E70
 #define SERVO_ARM_SPEED 0x010C
 #define SERVO_BUCKET_SPEED 0x010C
@@ -30,6 +30,7 @@ uint16_t readPin(unsigned int channel);
 void servoCallback(const std_msgs::UInt8::ConstPtr &msg);
 void liftDirt();
 void servoInit();
+void raiseBucket();
 
 SerialStream maestro;
 
@@ -90,7 +91,9 @@ uint16_t readPin(unsigned int channel)
 
 void liftDirt()
 {
-    char cmd_pos_bucket[] = {0x84, SERVO_BUCKET, (SERVO_BUCKET_RAISE_POS & 0x7F), ((SERVO_BUCKET_RAISE_POS << 7) & 0x7F) };
+    char cmd_pos_bucket[] = {0x84, SERVO_BUCKET, 
+                                  (SERVO_BUCKET_LIFT_POS & 0x7F), 
+                                 ((SERVO_BUCKET_LIFT_POS << 7) & 0x7F) };
     maestro.write(cmd_pos_bucket, sizeof(cmd_pos_bucket));
     ROS_INFO("Lifting dirt");
 }
@@ -98,8 +101,10 @@ void liftDirt()
 void raiseBucket()
 {
     char cmd_pos_arms[] = {0x9F, 2,
-                            SERVO_ARM_L, (SERVO_ARM_RAISE_POS & 0x7F), ((SERVO_ARM_RAISE_POS << 7) & 0x7F), 
-                                         (SERVO_ARM_RAISE_POS & 0x7F), ((SERVO_ARM_RAISE_POS << 7) & 0x7F), 
+                            SERVO_ARM_L, (SERVO_ARM_RAISE_POS & 0x7F), 
+                                        ((SERVO_ARM_RAISE_POS << 7) & 0x7F), 
+                                         (SERVO_ARM_RAISE_POS & 0x7F), 
+                                        ((SERVO_ARM_RAISE_POS << 7) & 0x7F), 
                           };
                     
     maestro.write(cmd_pos_arms, sizeof(cmd_pos_arms));
@@ -109,22 +114,33 @@ void raiseBucket()
 
 void servoInit()
 {
-    char cmd_speed_larm[] = {0x87, SERVO_ARM_L, (SERVO_ARM_SPEED & 0x7F), ((SERVO_ARM_SPEED << 7) & 0x7F)};
+    char cmd_speed_larm[] = {0x87, SERVO_ARM_L, (SERVO_ARM_SPEED & 0x7F), 
+                                                ((SERVO_ARM_SPEED << 7) & 0x7F)};
+    
     maestro.write(cmd_speed_larm, sizeof(cmd_speed_larm));
     
-    char cmd_speed_rarm[] = {0x87, SERVO_ARM_R, (SERVO_ARM_SPEED & 0x7F), ((SERVO_ARM_SPEED << 7) & 0x7F)};
+    char cmd_speed_rarm[] = {0x87, SERVO_ARM_R, 
+                                  (SERVO_ARM_SPEED & 0x7F), 
+                                 ((SERVO_ARM_SPEED << 7) & 0x7F)};
+    
     maestro.write(cmd_speed_rarm, sizeof(cmd_speed_rarm));
     
     char cmd_pos_arms[] = {0x9F, 2, 
-                            SERVO_ARM_L, (SERVO_ARM_BASE_POS & 0x7F), ((SERVO_ARM_BASE_POS << 7) & 0x7F), 
-                                         (SERVO_ARM_BASE_POS & 0x7F), ((SERVO_ARM_BASE_POS << 7) & 0x7F), 
+                            SERVO_ARM_L, (SERVO_ARM_BASE_POS & 0x7F), 
+                                        ((SERVO_ARM_BASE_POS << 7) & 0x7F), 
+                                         (SERVO_ARM_BASE_POS & 0x7F), 
+                                        ((SERVO_ARM_BASE_POS << 7) & 0x7F), 
                           };
     maestro.write(cmd_pos_arms, sizeof(cmd_pos_arms));
     
-    char cmd_speed_bucket[] = {0x87, SERVO_BUCKET, (SERVO_BUCKET_SPEED & 0x7F), ((SERVO_BUCKET_SPEED << 7) & 0x7F) };
+    char cmd_speed_bucket[] = {0x87, SERVO_BUCKET, 
+                                    (SERVO_BUCKET_SPEED & 0x7F), 
+                                   ((SERVO_BUCKET_SPEED << 7) & 0x7F) };
     maestro.write(cmd_speed_bucket, sizeof(cmd_speed_bucket));
     
-    char cmd_pos_bucket[] = {0x84, SERVO_BUCKET, (SERVO_BUCKET_BASE_POS & 0x7F), ((SERVO_BUCKET_BASE_POS << 7) & 0x7F) };
+    char cmd_pos_bucket[] = {0x84, SERVO_BUCKET, 
+                                  (SERVO_BUCKET_BASE_POS & 0x7F), 
+                                 ((SERVO_BUCKET_BASE_POS << 7) & 0x7F) };
     maestro.write(cmd_pos_bucket, sizeof(cmd_pos_bucket));
 }
 
